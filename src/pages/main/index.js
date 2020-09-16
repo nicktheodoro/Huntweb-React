d43e53848499e90205572f52ae72f6b1a14123e9
Nicolas Theodoro <nicolastheodoro@gmail.com>
2020-09-16 16:10:44 -0300

    showing product details and finalizing routes

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
@@ -61,6 +61,6 @@ export default class Main extends Component {
                 <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próximo</button>
             </div>
         </div>
-    )
+    );
     }
 }
